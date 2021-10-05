ee918b098546b5b0566d6390d71e6a75ee745077
Cyril Danilevski <cydanil@gmail.com>
2021-10-06 01:00:32 +0200

    Update interface domain in all ui files

--- src/gourmand/i18n.py
+++ src/gourmand/i18n.py
@@ -4,9 +4,11 @@ This file defines a gettext-like interface with the gourmet domain, and is
 later imported in the rest of the application.
 """
 import gettext
+import locale
 from pathlib import Path
 
 mo_path = Path(__file__).parent / 'data' / 'locale'
 
+locale.bindtextdomain('gourmand', mo_path)
 langs = gettext.translation('gourmand', mo_path, fallback=True)
 _ = langs.gettext
