89aba573e57d5bd9acbb9f8bc2b682e1fa9fe205
James Addison <55152140+jayaddison@users.noreply.github.com>
2025-02-27 16:23:36 +0000

    Tests: reactivate two more skipped tests (#182)

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,7 +1,5 @@
 import unittest
 
-import pytest
-
 from gourmand import convert
 
 
@@ -13,7 +11,6 @@ class ConvertTest(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(self.c.convert_simple("c", "c"), 1)
 
-    @pytest.mark.skip("Broken as of 20220813")
     def test_density(self):
         self.assertEqual(self.c.convert_w_density("ml", "g", item="water"), 1)
         self.assertEqual(self.c.convert_w_density("ml", "g", density=0.5), 0.5)
@@ -46,7 +43,6 @@ class ConvertTest(unittest.TestCase):
         ]:
             self.assertEqual(convert.frac_to_float(s), n)
 
-    @pytest.mark.skip("Broken as of 20220813")
     def test_ingmatcher(self):
         for s, a, u, i in [
             ("1 cup sugar", "1", "cup", "sugar"),
