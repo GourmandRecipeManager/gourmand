78f4fcc254b66dafa799fc029673b90091cc9396
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2025-09-13 10:36:43 +0200

    sync test name with function under test

--- tests/test_mycookbook_exporter.py
+++ tests/test_mycookbook_exporter.py
@@ -4,7 +4,7 @@ from gourmand.plugins.import_export.mycookbook_plugin.mycookbook_exporter import
 
 
 class RectoMCBTest(unittest.TestCase):
-    def test_sanitized_name(self):
+    def test_sanitize_image_name(self):
         input_name = "Brownies w/ caram/el"
         result = sanitize_image_name(input_name)
         self.assertNotIn('/', result)  # add assertion here
