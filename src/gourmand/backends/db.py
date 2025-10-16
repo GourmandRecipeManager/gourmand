56949ea5a9c21429cd977fee2743b5142a35fa74
holzc-oregonstate <107905575+holzc-oregonstate@users.noreply.github.com>
2025-10-16 09:24:39 -0700

    Bugfix: Import can handle recipe with category = None (#234)
    Closes #138.

--- src/gourmand/backends/db.py
+++ src/gourmand/backends/db.py
@@ -1266,7 +1266,7 @@ class RecData(Pluggable):
         and returns the entry in the database as a RowProxy.
         """
         cats = []
-        if "category" in dic:
+        if dic.get('category'):
             cats = [v.strip() for v in dic["category"].split(",") if v]
             del dic["category"]
         if "servings" in dic:
